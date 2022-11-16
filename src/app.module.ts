c52c2fc4d48d20376379878b3b888d5ce0eddc81
Saad Mehmood <saad.mehmood@aquila360.com>
2022-11-16 15:28:40 +0500

    project setup

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://saad:MZBpZmWQnr2ntYEv@cluster0.bgu3fsr.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
